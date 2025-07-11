cb548c00dc4dc6b430474f0f2576b5bb87cf364d
SugamChaudharry <nextgenachieverss@gmail.com>
2025-07-11 10:00:00 +0530

    feat: migrate db config and index

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index";
+import { app } from "./app";
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error: any) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+    app.listen(PORT || 8000, () => {
+      console.log(` server is running at Port :🫡:🫡: ${PORT} :🫡:🫡:`);
+    });
+  })
+  .catch((err: any) => {
+    console.log("MongoDB connection Failed !!! ", err);
+  });
+
+// in this approch we make iife function and for connecting database we have use good approch by using try and catch for error handling and async await for wating for data to come form other contnent
+/*
+import express from "express";
+const app = express()
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening to port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("Error: ",error);
+        throw error
+    }
+} )()
+
+*/
